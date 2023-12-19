49ea9ea23557f45016d4907e53dc3d27146e0896
DC-Chitoge <anhhao0602@gmail.com>
2023-12-19 22:27:03 +0700

    [#8] fixed

--- frontend/src/Component/UserListScreen.js
+++ frontend/src/Component/UserListScreen.js
@@ -39,6 +39,7 @@ const reducer = (state, action) => {
   }
 };
 export default function UserListScreen() {
+  const navigate = useNavigate();
   const [{ loading, error, users, loadingDelete, successDelete }, dispatch] =
     useReducer(reducer, {
       loading: true,
